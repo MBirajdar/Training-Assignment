3f506924021b7d9343dd96ce6a205591fb9a16f9
Manoj Birajdar <m.birajdar@zensar.com>
2022-04-24 17:48:28 +0530

    assignment

---
+++ Assignment no 2/Assignment no 2/Assignment no 2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_no_2
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            // 1, avarage maximum and minimum.
+            Console.WriteLine("Q no 1");
+            int[] Num = { 10, 20, 30, 40, 50 };
+            Console.WriteLine("(10,20,30,40,50)");
+            Console.WriteLine(" The avarage element :" + " " + Num.Average());
+            Console.WriteLine(" The largest element :" + " " + Num.Max());
+            Console.WriteLine(" The smallest element :" + " " + Num.Min());
+
+
+
+            // marks avarage, maximum and minimum, sum of all marks, accending , decending order.
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.WriteLine("Q no 2");
+            int[] m = { 30,40,60,70,50,80,20 };
+            Console.WriteLine("(marks: 30,40,60,70,50,80,20)");
+
+            Console.WriteLine(" Minimum Marks :" + " " + m.Min());
+            Console.WriteLine(" Maximum Marks :" + " " + m.Max());
+            Console.WriteLine(" Avarage Marks:" + " " + m.Average());
+            Console.WriteLine("Total Marks: " + m.Sum());
+
+            Array.Sort(m);
+            Console.WriteLine("array ascending order...");
+            foreach (int i in m)
+            {
+                Console.WriteLine(i + " ");
+            }
+            Array.Reverse(m);
+            Console.WriteLine("array decending order...");
+            foreach (int i in m)
+            {
+                Console.WriteLine(i + " ");
+            }
+
+
+            // reverse string
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.WriteLine("Q no 3-A");
+            Console.WriteLine(" Enter  a string ");
+            string Orginal = Console.ReadLine();
+            string Reverse = string.Empty;
+
+            for (int i = Orginal.Length - 1; i >= 0; i--)
+            {
+                Reverse += Orginal[i];
+            }
+            Console.WriteLine(" REVERESE STRING IS : " + Reverse);
+
+            // both words are same or not
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.WriteLine("Q no 3-B");
+            Console.WriteLine("(two words are same or not)");
+            Console.WriteLine(" Enter  a word 1:- ");
+            string s1 = Console.ReadLine();
+            Console.WriteLine(" Enter  a word 2:- ");
+            string s2 = Console.ReadLine();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine(" both words are same ");
+            }
+            else if (s2 != s1)
+            {
+                Console.WriteLine(" both words are  not same ");
+            }
+           
+
+            // lenght of string
+
+            Console.WriteLine("----------------------------------------------------------------------------");
+            Console.WriteLine("Q no 3-C");
+            Console.WriteLine("Length of string");
+            Console.WriteLine("enter the string");
+            string s = Console.ReadLine();
+            Console.WriteLine(s.Length);
+            Console.ReadLine();
+
+            // To check whether the given word is a palindrome or not. 
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine("Q no 4");
+            Console.WriteLine("Enter the word");
+            string inputStr = Console.ReadLine();
+            char[] letter = inputStr.ToCharArray();
+            Array.Reverse(letter);
+            string r;
+            r = new string(letter);
+            bool bool1 = inputStr.Equals(r, StringComparison.OrdinalIgnoreCase);
+            if (bool1 == true)
+            {
+                Console.WriteLine("This string " + inputStr + " is a Palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("This string " + inputStr + " is not a Palindrome!");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Assignment no 3/Assignment no 3/Assignment no 3/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_no_3
+{
+    internal class Student
+    {
+        //Display of marks--------------
+        int rollno;
+        String name;
+        String branch;
+        String sem;
+        String clas;
+
+        //static void Main(string[] args)
+        static public void displayResult()
+        {
+            Console.WriteLine("Q no 1");
+            int[] marks = new int[5];
+            marks[0] = 20;
+            marks[1] = 40;
+            marks[2] = 60;
+            marks[3] = 80;
+            marks[4] = 90;
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine("the marks of " + i + " subs are: " + marks[i]);
+            }
+
+            double average = marks.Average();
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                if (marks[i] < 35)
+                {
+                    Console.WriteLine("FAILED");
+                }
+                if (marks[i] > 35 && average < 50)
+                {
+                    Console.WriteLine("FAILED");
+                }
+                if  (average > 50)
+                {
+                    Console.WriteLine("PASSED");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID");
+                }
+
+            }
+        }
+
+        public Student(int rollno, String name, String branch, String sem, String clas)
+        {
+
+            this.rollno = rollno;
+            this.name = name;
+            this.branch = branch;
+            this.sem = sem;
+            this.clas = clas;
+            Console.WriteLine($"\nRollno is: {rollno}, Name is {name},Student class is {clas}, Sem is {sem},Branch is : {branch}");
+        }
+        static public void displayData()
+        {
+           // Console.WriteLine("\nthe marks: ");
+        }
+        //car details-------------
+
+        internal class Car
+        {
+            int car_no;
+            string car_name;
+            string car_type;
+            readonly double cost = 700000;
+            public Car(int car_no, string car_name, string car_type, double cost)
+            {
+                Console.WriteLine("Q no 2");
+                this.car_no = car_no;
+                this.car_name = car_name;
+                this.car_type = car_type;
+                this.cost = cost;
+                Console.WriteLine($"\nthe car_no is {car_no},car_name is {car_name},car_type is {car_type},cost is {cost} ");
+            }
+            public void showDetails()
+            {
+                Console.WriteLine("\nbefore changing");
+                Console.WriteLine($"the car_no is {car_no},car_name is {car_name},car_type is {car_type},cost is {cost} ");
+            }
+            public Car(double car_cost)
+            {
+                Console.WriteLine("\nafter changing");
+                car_cost = cost;
+                Console.WriteLine($"the car_no is {car_no},car_name is {car_name},car_type is {car_type},cost is {cost} ");
+                Console.ReadLine();
+            }
+
+        }
+        internal class bank
+        {
+            double interestRate;
+            public void loanCalculator(double loanamt)
+            {
+                
+                Console.WriteLine("\nPlease enter rate of interest");
+                interestRate = Convert.ToDouble(Console.ReadLine());
+                double interest = (loanamt * interestRate) / 100;
+                Console.WriteLine("\nThe interest on given ammount    is : " + interest);
+            }
+
+        }
+
+
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                //===============student=============
+                Student.displayResult();
+                Student student = new Student(10, "Manoj", "Mechanical", "8th", "BE");
+                Student.displayData();
+
+                //==============car=============
+                Car car = new Car(123, "Lamborghini", "sports", 1500000);
+                car.showDetails();
+                Car car2 = new Car(10000000);
+
+                //=============bank===============
+                bank bank = new bank();
+                Console.Write("Q no 3");
+                Console.WriteLine("\nenter the loan amount: ");
+                bank.loanCalculator(Convert.ToInt32(Console.ReadLine()));
+
+                Console.ReadLine();
+
+
+
+
+            }
+        }
+    }
+}
+
+
+            
+        
