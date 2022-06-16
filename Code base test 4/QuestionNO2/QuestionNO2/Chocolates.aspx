11d8cbd4ce5f5c312bca63fb6a2fecdb6b401697
Manoj Birajdar <m.birajdar@zensar.com>
2022-06-16 18:32:19 +0530

    Codebase4

---
+++ Code base test 4/QuestionNO2/QuestionNO2/Chocolates.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Chocolates.aspx.cs" Inherits="QuestionNO2.Chocolates" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+     <form id="form1" runat="server"  >
+<div>
+<h1 style=" color:deepskyblue"><center>CHOCOLATES</center></h1>
+<br />
+</div>
+<h2 style="color:black;"><center>  CHOCOLATES: <asp:DropDownList ID="DropDownList1" runat="server" Height="25px" Width="180px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True">
+<asp:ListItem></asp:ListItem>
+
+</asp:DropDownList></center></h2>
+<br />
+
+<center><asp:Image ID="Image1" runat="server" Height="225px" Width="275px" /></center>
+<br />
+<%--<asp:Button ID="Button1" runat="server" Text="Click to Check the Price" />--%>
+<h2 style="color:purple;"><center>
+<asp:Label ID="Label" runat="server" Text="Price: "></asp:Label></center></h2>
+</form>
+</body>
+</html>
---
+++ Code base test 4/QuestionNO2/QuestionNO2/Chocolates.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QuestionNO2
+{
+    public partial class Chocolates : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs m)
+        {
+            if (!Page.IsPostBack)
+            {
+
+
+
+                string[] str = new string[] { "dairy milk", "DarkChocolate", "kit kat", "munch chocolate" };
+                for (int i = 0; i < str.Length; i++)
+                {
+                    DropDownList1.Items.Add(str[i]);
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs m)
+        {
+            string str = DropDownList1.Text;
+            Image1.ImageUrl = "~/Images/" + str + ".JFIF";
+            if (DropDownList1.Items.FindByText("dairy milk").Selected == true)
+            {
+                Label.Text = "Price: 550/-";
+            }
+            else if (DropDownList1.Items.FindByText("DarkChocolate").Selected == true)
+            {
+                Label.Text = "Price: 999/-";
+            }
+            else if (DropDownList1.Items.FindByText("kit kat").Selected == true)
+            {
+                Label.Text = "Price: 450/-";
+            }
+
+            else
+            {
+                Label.Text = "Price: 250/-";
+            }
+        }
+    }
+}
